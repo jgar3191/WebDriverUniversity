bda4f721a4c9a6d89b34dd8f0af70c3335874eb0
Jorge G. Alvarado R <jorgegerardo.alvardorubio@gmail.com>
2022-07-07 03:24:39 -0500

    Parallel Execution

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,6 +2,7 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"classpath:features"},
@@ -12,4 +13,10 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"}
 )
 public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+        public Object[][] scenarios(){
+                return super.scenarios();
+    }
+
 }
